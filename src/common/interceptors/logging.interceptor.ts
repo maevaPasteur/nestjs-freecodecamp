f25810b6d7efa02b6e8a3180649bd878326aa648
maevaPasteur <maeva.pasteur.pro@gmail.com>
2025-10-29 14:32:11 +0400

    feat(interpectors): logging

---
+++ src/common/interceptors/logging.interceptor.ts
@@ -0,0 +1,38 @@
+import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } from "@nestjs/common";
+import { Observable, tap } from "rxjs";
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  private readonly logger = new Logger();
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest();
+    const { method, url, body, query, param } = request;
+    const userAgent = request.get('user-agent') || 'unknown';
+    const userId = request?.user?.id || 'unauthenticated';
+    this.logger.log(`
+    [${method} ${url} - User: ${userId} - User Agent: ${userAgent}}]
+    `);
+    const startTime = Date.now();
+
+    //tag operator allow to perform side effects
+    return next.handle().pipe(
+      tap({
+        next: (data) => {
+          const endTime = Date.now();
+          const duration = endTime - startTime;
+          this.logger.log(`
+          [${method} ${url}: ${duration}ms - Response size ${JSON.stringify(data)?.length || 0} bytes]
+          `)
+        },
+        error: (error) => {
+          const endTime = Date.now();
+          const duration = endTime - startTime;
+          this.logger.log(`
+          [${method} ${url} - ${duration}ms - Error ${error.message}]
+          `)
+        }
+      })
+    )
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,29 @@
-import {NestFactory, Reflector} from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {ClassSerializerInterceptor, ValidationPipe} from "@nestjs/common";
+import { ClassSerializerInterceptor, Logger, ValidationPipe } from "@nestjs/common";
+import { LoggingInterceptor } from "./common/interceptors/logging.interceptor";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug', 'verbose']
+  });
 
   app.useGlobalPipes(
-      new ValidationPipe({
-        whitelist: true, // stripe properties without decorator,
-        forbidNonWhitelisted: true, // reject request if unauthorized properties
-        transform: true, // automatically transform payload into object type from dto class
-        disableErrorMessages: false,
-      })
+    new ValidationPipe({
+      whitelist: true, // stripe properties without decorator,
+      forbidNonWhitelisted: true, // reject request if unauthorized properties
+      transform: true, // automatically transform payload into object type from dto class
+      disableErrorMessages: false,
+    })
   )
 
-    app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalInterceptors(
+    new LoggingInterceptor(),
+    new ClassSerializerInterceptor(app.get(Reflector))
+  );
 
-    await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
